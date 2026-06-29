0899759a50b432e1324ab6de22fffb9828b27dd9
manthan <manthanranderi4@gmail.com>
2026-06-29 12:01:35 +0530

    Update MongoDB connection string for db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/adminPanel');
+mongoose.connect('mongodb://manthanranderi4_db_user:blog2730@ac-hpzzchc-shard-00-00.km9nayu.mongodb.net:27017,ac-hpzzchc-shard-00-01.km9nayu.mongodb.net:27017,ac-hpzzchc-shard-00-02.km9nayu.mongodb.net:27017/?ssl=true&replicaSet=atlas-bovvr1-shard-0&authSource=admin&appName=blog');
 
 const db = mongoose.connection;
 
